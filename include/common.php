193ddb93dbdb3d76db59fbc89638d32bacba0b2b
simshengqin <54699127+simshengqin@users.noreply.github.com>
2020-09-27 17:29:39 +0800

    Added common.php and protect.php

---
+++ include/common.php
@@ -0,0 +1,75 @@
+<?php
+
+// this will autoload the class that we need in our code
+spl_autoload_register(function($class) {
+ 
+    // we are assuming that it is in the same directory as common.php
+    // otherwise we have to do
+    // $path = 'path/to/' . $class . ".php"    
+    require_once "$class.php"; 
+  
+});
+//loads all the necessary bootstrap, ajax and link to main.css
+echo '
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="main.css">
+';
+// session related stuff
+
+session_start();
+
+
+function printErrors() {
+    if(isset($_SESSION['errors'])){
+        echo "<ul id='errors' style='color:red;'>";
+        
+        foreach ($_SESSION['errors'] as $value) {
+            echo "<li>" . $value . "</li>";
+        }
+        
+        echo "</ul>";   
+        unset($_SESSION['errors']);
+    }    
+}
+
+
+function isMissingOrEmpty($name) {
+    if (!isset($_REQUEST[$name])) {
+        return "blank $name";
+    }
+
+    // client did send the value over
+    $value = $_REQUEST[$name];
+    if (empty($value)) {
+        return "blank $name";
+    }
+}
+
+# check if an int input is an int and non-negative
+function isNonNegativeInt($var) {
+    if (is_numeric($var) && $var >= 0 && $var == round($var))
+        return TRUE;
+}
+
+# check if a float input is is numeric and non-negative
+function isNonNegativeFloat($var) {
+    if (is_numeric($var) && $var >= 0)
+        return TRUE;
+}
+
+# this is better than empty when use with array, empty($var) returns FALSE even when
+# $var has only empty cells
+function isEmpty($var) {
+    if (isset($var) && is_array($var))
+        foreach ($var as $key => $value) {
+            if (empty($value)) {
+               unset($var[$key]);
+            }
+        }
+
+    if (empty($var))
+        return TRUE;
+}
