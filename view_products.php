063b6bc9e2ffc8a5b74d76010a233e401d09546f
simshengqin <54699127+simshengqin@users.noreply.github.com>
2020-10-04 10:19:48 +0800

    Update css files and view_products.php

--- view_products.php
+++ view_products.php
@@ -3,14 +3,19 @@
   require_once 'include/protect.php';
 
 ?>
+<head>
+<title>View Products</title>
+<!-- Roboto Font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+<!--Bootstrap 4 and AJAX-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' />
-<link rel='stylesheet' href='include\main.css'>
+<!--Link to main.css files while contains all the css of this project-->
+<link rel='stylesheet' href='css\maincss.css'>
 <div class="jumbotron color-grey-light">
     <div class="d-flex align-items-center h-20">
         <div class="container text-center py-5">
@@ -18,11 +23,13 @@
         </div>
     </div>
 </div>
+</head>
+<body>
 <div class='container-fluid'>
     <div class='row'>
         <div name="toastdiv">
             <!--Toast, which is a message pop-up whenever an item is added to the cart-->
-            <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+            <div style="position: relative; min-height: 200px;">
             <!-- Position it -->
             <div style="position: fixed; top: 0; right: 0;  z-index: 10;" >
 
@@ -331,4 +338,5 @@
         //alert('Successfully added ' + name + ' to cart!');
     }
 </script>
+</body>
 
