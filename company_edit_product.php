fb7a97449b620721a3e1e24a9ce5169af3ce75d4
VictorWeiLong <53215558+VictorWeiLong@users.noreply.github.com>
2020-10-30 02:08:08 +0800

    Update to Company

--- company_edit_product.php
+++ company_edit_product.php
@@ -25,6 +25,7 @@
         header("Location: company_edit_product.php#{$id}");
       }
     }
+    
     if(isset($_POST['deleteProduct']))
     {
       $id = $_POST["productid"];
@@ -63,58 +64,20 @@
 
 <body>
 
-   
-
-<!-- Navigation Bar -->
-<!--
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container">
-    <a class="navbar-brand" href="mainpage.html">Eco</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active mr-4">
-          <a class="nav-link" href="company_profile.php"> Dashboard <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item mr-4">
-          <a class="nav-link" href="company_post_product.php"> Post </a>
-        </li>
-        <li class="nav-item mr-4">
-          <a class="nav-link" href="company_edit_product.php"> Edit </a>
-        </li>
-      </ul>
-    </div>
-    </div>
-  </nav>
--->
 <?php include 'include/company_navbar.php';?>
 
   <!--Company profile  -->
-
-
 
   <div class="jumbotron jumbotron-fluid bg-light">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                   </br>
-                  <h1 class="text-center font-weight-light"> Edit </h1>
-                  </br>
-                  </br>
-
-                    <!-- End of Modal -->
-
-                    <?php displayProducts($company_id) ?>
-
+                  <h1 class="text-center font-weight-light"> Edit Promotion </h1>
+                  <?php displayProducts($company_id);?>
             </div>
-
         </div>
     </div>
-
-
-
 
     <!-- Footer -->
   <footer class="py-5">
@@ -158,7 +121,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var num = ele.getAttribute('value');
         num = parseFloat(num);
         num = num.toFixed(2)
-        console.log(num);
+        //console.log(num);
         ele.setAttribute('value', num);
       }
 
@@ -167,21 +130,118 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var num = ele.getAttribute('value');
         num = parseFloat(num);
         num = num.toFixed(2)
-        console.log(num);
+        //console.log(num);
         ele.setAttribute('value', num);
       }
 
+    var data = document.getElementsByClassName("dateTimeInputLeft");
+    for(var ele of data){
+      var id_Date_Time = ele.getAttribute('value');
+    
+      var productid = id_Date_Time.split("*")[0];
+      var decay_date_Unmodified = id_Date_Time.split("*")[1];
+      var decay_time_Unmodified = id_Date_Time.split("*")[2];
+      // date
+      var year = decay_date_Unmodified.split("-")[0];
+      var month = decay_date_Unmodified.split("-")[1];
+      var day = decay_date_Unmodified.split("-")[2];
+      // convert month num into month text
+      var monthList = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+      month = month -1;
+      var monthText = "";
+      for(var i = 0; i < monthList.length; i++){
+        if(month == i){
+          monthText = monthList[i];
+        }
+      }
+      // time
+      var decay_time_Unmodified_without_milliseconds = decay_time_Unmodified.split(".")[0];
+      var hour = decay_time_Unmodified_without_milliseconds.split(':')[0];
+      var min = decay_time_Unmodified_without_milliseconds.split(':')[1];
+      var sec = decay_time_Unmodified_without_milliseconds.split(':')[2];
+      // date time input into string format
+      var newTime = " " + monthText + " " + day + ", " + year + " " + hour + ":" + min + ":" + sec + "";
+      
+      var countDownDate = new Date(newTime).getTime();
+      var now = new Date().getTime();
+      var timeRemaining =  countDownDate - now;
+
+      if (timeRemaining < 0) {
+        var newValue = document.getElementById('name_'+productid).getAttribute('value');
+        newValue = newValue + " * EXPIRED";
+        document.getElementById('name_'+productid).setAttribute('value', newValue);
+        document.getElementById('name_'+productid).style.color = "red";
+      }
+    }
+  
+
 });
 
+function validationUpdate(errorDateInput, decay_date_value, errorEndTime, decay_time_value, errorAfterPrice, price_after_value, errorQuantity, quantity_value){
+    var noError = true;
 
-function deleteModal(){
-      console.log("Test");
-  }
+    errorDateInput = errorDateInput.attributes[0].value
+    errorEndTime = errorEndTime.attributes[0].value
+    errorAfterPrice = errorAfterPrice.attributes[0].value
+    errorQuantity = errorQuantity.attributes[0].value
+
+    var decayDateInput = document.getElementById(decay_date_value.attributes[1].value).value;
+    var decayTimeInput = document.getElementById(decay_time_value.attributes[1].value).value;
+    var priceAfter = document.getElementById(price_after_value.attributes[1].value).value;
+    var qty = document.getElementById(quantity_value.attributes[1].value).value;
+
+    if(decayDateInput==""){
+      document.getElementById(errorDateInput).innerHTML = "Please specify a new end date."
+      document.getElementById(errorDateInput).style.visibility = "visible";
+      noError = false;
+    } else {
+      document.getElementById(errorDateInput).style.visibility = "hidden";
+    }
+
+    if(decayTimeInput==""){
+      document.getElementById(errorEndTime).innerHTML = "Please specify a new end time."
+      document.getElementById(errorEndTime).style.visibility = "visible";
+      noError = false;
+    } else {
+      document.getElementById(errorEndTime).style.visibility = "hidden";
+    }
+
+    if(priceAfter==""){
+      document.getElementById(errorAfterPrice).innerHTML = "Please specify a new price."
+      document.getElementById(errorAfterPrice).style.visibility = "visible";
+      noError = false;
+    } else if (priceAfter < 0.01){
+      document.getElementById(errorAfterPrice).innerHTML = "Price cannot be less than 0."
+      document.getElementById(errorAfterPrice).style.visibility = "visible";
+      noError = false;
+    } else {
+      document.getElementById(errorAfterPrice).style.visibility = "hidden";
+    }
+
+    if(qty==""){
+      document.getElementById(errorQuantity).innerHTML = "Please specify a new Qty."
+      document.getElementById(errorQuantity).style.visibility = "visible";
+      noError = false;
+    } else if (qty < 1){
+      document.getElementById(errorQuantity).innerHTML = "Qty cannot be less than 0."
+      document.getElementById(errorQuantity).style.visibility = "visible";
+      noError = false;
+    } else {
+      document.getElementById(errorQuantity).style.visibility = "hidden";
+    }
+
+    if(!noError){
+      return false
+    } else {
+      return true
+    }
+    return false;
+}
 
 
-  </script>    
+</script>    
 
-  </body>
+</body>
 
 <?php 
   function displayProducts($company_id) {
@@ -189,6 +249,18 @@ function deleteModal(){
       $products = $companyProducts->retrieve_product_by_company($company_id);
       foreach($products as $product){
         $time = convertTime($product->get_decay_time());
+
+        $strProductId = strval($product->get_product_id());
+
+        $errorDateInput = "errorDateInput".$strProductId;
+        $decay_date_value = "decayDate".$strProductId;
+        $errorEndTime = "errorEndTime".$strProductId;
+        $decay_time_value = "decayTime".$strProductId;
+        $errorAfterPrice = "errorAfterPrice".$strProductId;
+        $price_after_value = "priceAfter".$strProductId;
+        $errorQuantity = "errorQuantity".$strProductId;
+        $quantity_value = "quantity".$strProductId;
+
         echo"
         
         <h1 style='visibility: hidden;' id='{$product->get_product_id()}'> - </h1>
@@ -197,20 +269,19 @@ function deleteModal(){
 
           <div class='col-md-12 text-center'>
             <p style='visibility: hidden;'>  - </p>
+            <input type='hidden' class='dateTimeInputLeft' value='{$product->get_product_id()}*{$product->get_decay_date()}*{$product->get_decay_time()}'>
           </div>
 
           <div class='col-md-6 text-center ' style='padding-bottom: 30px;'>
               <!-- <img class='.img-fluid' style='max-width: 75%; height: auto' src='images/{$product->get_category()}/{$product->get_name()}.jpg'> -->
-              <img class='.img-fluid rounded shadow d-flex w-100' style='max-width: 90%; height: auto' src='{$product->get_image_url()}'>
-              </br>
-              </br>
-              </br>
-              <h3 class='card-title font-weight-light'>".strtoupper(str_replace('_', ' ', $product->get_name()))."</h3>
+              <img class='.img-fluid rounded shadow d-flex w-100 mx-auto' style='max-width: 80%; height: auto' src='{$product->get_image_url()}'>
           </div>
 
           <div class='col-md-6'>
             <form action='company_edit_product.php' method='POST'>
                 
+                <input type='text' readonly class='form-control-plaintext' id='name_{$product->get_product_id()}' value='".ucfirst(str_replace('_', ' ', $product->get_name()))."' style='font-size: 30px;'>
+                <!-- <h2 class='card-title font-weight-light' id='{$product->get_product_id()}_OLD'>".ucfirst(str_replace('_', ' ', $product->get_name()))."</h2> -->
 
                 <input type='hidden' name='errorBugBlock' value={$product->get_product_id()}>
 
@@ -221,20 +292,20 @@ function deleteModal(){
                         <div class='input-group-prepend'>
                             <span class='input-group-text' id='{$product->get_product_id()}date'> End Date </span>
                         </div>
-                        <input class='form-control form-control-lg dateInput' id='{$product->get_product_id()}_decay_date' name='decay_date' type='date' value='{$product->get_decay_date()}' aria-describedby='{$product->get_product_id()}date'>
-                        <p id='errorQuantity' style='visibility: hidden; color: red;'> </p>
+                        <input class='form-control form-control-lg dateInput' id='decayDate{$product->get_product_id()}' name='decay_date' type='date' value='{$product->get_decay_date()}' aria-describedby='{$product->get_product_id()}date'>   
                     </div>
                 </div>
+                <p id='errorDateInput{$product->get_product_id()}' value='test' style='visibility: hidden; color: red;'> </p>
 
                 <div class='form-group'>
                     <div class='input-group mb-3'>
                         <div class='input-group-prepend'>
                             <span class='input-group-text' id='{$product->get_product_id()}time'> End Time </span>
                         </div>
-                        <input class='form-control form-control-lg timeInput' id='{$product->get_product_id()}_decay_time' name='decay_time' type='time' value='{$time}' min='00:00:00' max='23:59:59' aria-describedby='{$product->get_product_id()}time'>
-                        <p id='errorQuantity' style='visibility: hidden; color: red;'> </p>
+                        <input class='form-control form-control-lg timeInput' id='decayTime{$product->get_product_id()}' name='decay_time' type='time' value='{$time}' min='00:00:00' max='23:59:59' aria-describedby='{$product->get_product_id()}time'>
                     </div>
                 </div>
+                <p id='errorEndTime{$product->get_product_id()}' style='visibility: hidden; color: red;'> </p>
 
                 <div class='form-group' style='margin-bottom:-15px;'>
                   <div class='input-group mb-3'>
@@ -249,22 +320,23 @@ function deleteModal(){
                         <div class='input-group-prepend'>
                             <span class='input-group-text' id='{$product->get_product_id()}afterprice'> After Price $</span>
                         </div>
-                        <input class='form-control form-control-lg' id='{$product->get_product_id()}_price_after' name='price_after' type='double' value='{$product->get_price_after()}' aria-describedby='{$product->get_product_id()}afterprice'>
-                        <p id='errorQuantity' style='visibility: hidden; color: red;'> </p>
+                        <input class='form-control form-control-lg' id='priceAfter{$product->get_product_id()}' name='price_after' type='double' value='{$product->get_price_after()}' min='1' aria-describedby='{$product->get_product_id()}afterprice'>
                     </div>
                 </div>
+                <p id='errorAfterPrice{$product->get_product_id()}' style='visibility: hidden; color: red;'> </p>
 
                 <div class='form-group'>
                     <div class='input-group mb-3'>
                         <div class='input-group-prepend'>
                             <span class='input-group-text' id='{$product->get_product_id()}qty'> Quantity </span>
                         </div>
-                        <input class='form-control form-control-lg' id='{$product->get_product_id()}_quantity' name='quantity' type='double' value='{$product->get_quantity()}' aria-describedby='{$product->get_product_id()}qty'>
-                        <p id='errorQuantity' style='visibility: hidden; color: red;'> </p>
+                        <input class='form-control form-control-lg' id='quantity{$product->get_product_id()}' name='quantity' type='double' value='{$product->get_quantity()}' min='1' aria-describedby='{$product->get_product_id()}qty'>
                     </div>
                 </div>
+                <p id='errorQuantity{$product->get_product_id()}' style='visibility: hidden; color: red;'> </p>
 
-                <button type='submit' class='btn btn-info btn-lg btn-block' name='editProduct' style='left: 0%;'> Update </button>
+                
+                <button type='submit' class='btn btn-info btn-lg btn-block' name='editProduct' style='left: 0%;' onclick='return validationUpdate($errorDateInput, $decay_date_value, $errorEndTime, $decay_time_value, $errorAfterPrice, $price_after_value, $errorQuantity, $quantity_value)'> Update </button>
                 <button type='submit' class='btn btn-danger btn-lg btn-block' name='deleteProduct' style='left: 0%;'> Delete </button>
 
                 </br>
