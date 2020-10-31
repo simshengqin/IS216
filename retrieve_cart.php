8424d7d8bfba137f7ca63c8ccf4d66287864eef2
simshengqin <sqsim.2018@sis.smu.edu.sg>
2020-11-01 02:23:30 +0800

    change_back

--- retrieve_cart.php
+++ retrieve_cart.php
@@ -5,9 +5,9 @@ if(isset($_SESSION['user_id'])) {
     
     //companyDAO is for retrieving the and company image
     $userDAO = new userDAO();
-    $cart_company_id = $userDAO -> retrieve_cart_company_id($user_id);
+    //$cart_company_id = $userDAO -> retrieve_cart_company_id($user_id);
     $cart = $userDAO -> retrieve_user_cart($user_id);
-    
+    //$cart_and_cart_company_id = $userDAO -> retrieve_cart_company_id($user_id);
   
     echo "$cart";
 
