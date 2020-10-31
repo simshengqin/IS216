00a526330016134e6d754316ccd1473cbc5a4488
simshengqin <sqsim.2018@sis.smu.edu.sg>
2020-10-31 18:28:09 +0800

    new

--- inbox.php
+++ inbox.php
@@ -107,7 +107,7 @@
         update_selected_messages();
          //Update the left side bar messages ever 1 second
         update_selected_messages_leftbar();
-    }, 500);
+    }, 10000);
 
     function update_selected_messages_leftbar() {
         //Update the left sidebar messages
