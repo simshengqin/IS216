67bf8397748f17ab562bfe58fbf3b561524da9c7
ZiRong27 <zirong.tan.2018@sis.smu.edu.sg>
2020-11-13 09:28:52 +0800

    Removed link to index.php for company navbar

--- app/include/company_navbar.php
+++ app/include/company_navbar.php
@@ -33,7 +33,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-    <a class="navbar-brand" href="index.php"><img src="images/logo/rsz_e (1).png">    <img src="images/logo/rsz_shadow_eco.png"></a>
+    <a class="navbar-brand"><img src="images/logo/rsz_e (1).png">    <img src="images/logo/rsz_shadow_eco.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
