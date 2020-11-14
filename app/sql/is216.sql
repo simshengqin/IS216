8eb30fcd6739c937987c178dca092e4374cb626e
ZiRong27 <zirong.tan.2018@sis.smu.edu.sg>
2020-11-14 10:20:57 +0800

    deleted payments table

--- app/sql/is216.sql
+++ app/sql/is216.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 14, 2020 at 02:06 AM
+-- Generation Time: Nov 14, 2020 at 02:19 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
