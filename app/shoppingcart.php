f6d31aab13f72df640882eb3c922eb990cea9e1a
ZiRong27 <zirong.tan.2018@sis.smu.edu.sg>
2020-11-15 13:32:47 +0800

    Fixed mobile menu dropdown in shopping cart and removed some console.logs

--- app/shoppingcart.php
+++ app/shoppingcart.php
@@ -25,7 +25,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
   <!--Link to main.css files while contains all the css of this project-->
   <link rel='stylesheet' href='css\maincss.css'>
 
@@ -678,7 +678,7 @@
                     // so we use try-catch block to handle errors
                     // expected response is JSON data
                     var data = JSON.parse(this.responseText);
-                    console.log(data);
+                    
                     if ( data["status"] == "ZERO_RESULTS") {
                         document.getElementById("map").innerHTML = "<div class='alert alert-danger'>Invalid postal code. Please refresh the page </div>";
                     }
@@ -686,13 +686,13 @@
                     var loc = data["results"][0]["geometry"]["location"];
                     start_latitude = loc["lat"];
                     start_longtitude = loc["lng"];
-                    console.log(start_latitude, start_longtitude);
+                    
                     //Retrieves the company latitude and longtitude                        
                     end_latlng_arr = document.getElementById("map").getAttribute("name").split(",");                
                     end_latitude = end_latlng_arr[0]/10000000;
                     end_longtitude = end_latlng_arr[1]/10000000;
-                    console.log("End:", end_latitude, end_longtitude);  
-                    //console.log(window.latitude, window.longtitude);            
+                      
+                                
                     var directionsService = new google.maps.DirectionsService();
                     var directionsRenderer = new google.maps.DirectionsRenderer();
                     var start = new google.maps.LatLng(start_latitude,start_longtitude);
@@ -707,8 +707,7 @@
                         // "property."
                         travelMode: 'DRIVING' //google.maps.TravelMode[selectedMode]
                     };
-                    console.log(start);
-                    console.log(end);
+                    
 
                     directionsService.route(request, function(response, status) {
                         if (status == 'OK') {
@@ -717,7 +716,7 @@
                         else {
                           document.getElementById("map").innerHTML = "<div class='alert alert-danger'>Invalid postal code. Please refresh the page!</div>";
                         }
-                        console.log(response);
+                        
                     });  
                 }
             };
